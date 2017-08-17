348b8d6236addd950c9566557042d47b5dfea474
icode <intelligentcodemail@gmail.com>
2017-08-17 16:52:41 +0800

    fix: windows `startup.bat`

--- src/main/scripts/startup.bat
+++ src/main/scripts/startup.bat
@@ -6,11 +6,11 @@ SET VM_OPTIONS_FILE=%CURRENTDIR%\ameba.vmoptions
 SET ACC=
 FOR /F "usebackq delims=" %%i IN ("%VM_OPTIONS_FILE%") DO CALL "%CURRENTDIR%\append.bat" "%%i"
 
-SET NPN=
-FOR /R "%CURRENTDIR\lib" %i IN ("grizzly-npn-api-?.?.jar") DO SET NPN=%i
-
 cd..
 SET PARENTDIR=%cd%
+
+SET NPN=
+FOR /R "%PARENTDIR%\lib" %%i IN ("grizzly-npn-api-?.?.jar") DO SET NPN=%%i
 
 set LIB_CP=%PARENTDIR%;%PARENTDIR%\lib\*;
 
